52afe703aec4aae986a638d3bd7d811574a1bd98
AnhNguyet2003 <anhbnguyet@gmail.com>
2025-02-20 22:00:13 +0700

    update

--- backend/config/dbconnect.js
+++ backend/config/dbconnect.js
@@ -2,13 +2,14 @@
 
  const dbConnect = async () => {
     try {
+        console.log(process.env.MONGODB_URI)
         const conn = await mongoose.connect(process.env.MONGODB_URI)
         if (conn.connection.readyState == 1) console.log('DB connection is ready to connect')
         else console.log('Connection is not ready to connect')
 
     } catch (error) {
-        console.log('DB connection error')
-        throw new Error(error)
+        console.error('DB connection error:', error.message); // In ra thông báo lỗi chi tiết
+        throw new Error(error);
     }
  }
  module.exports = dbConnect 
